f1a493061901132e053941fe0801ffee1260a150
make-it-git <make.it.git@gmail.com>
2022-07-18 23:33:51 +0300

    HW8 completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"errors"
+	"io"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmd []string, env Environment, stdout, stderr io.Writer, stdin io.Reader) (returnCode int) {
+	c := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	c.Stdout = stdout
+	c.Stderr = stderr
+	c.Stdin = stdin
+
+	c.Env = prepareEnvironment(env)
+	err := c.Run()
+
+	var exitError *exec.ExitError
+	if errors.As(err, &exitError) {
+		return exitError.ExitCode()
+	}
+	if err != nil {
+		return -1
+	}
+
+	return 0
 }
